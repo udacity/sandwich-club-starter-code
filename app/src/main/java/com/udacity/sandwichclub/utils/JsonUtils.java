0431a5ddd69e7167758d7b4dc9f81a99d9d8f204
Matt <mbeimcik@gmail.com>
2018-02-22 11:02:24 +0100

    parsing JSON

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,11 +2,52 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        //TODO implement parsing here
+        try {
+            JSONObject sandwich = new JSONObject(json);
+            JSONObject name = sandwich.getJSONObject(JSONStructure.NAME.JSON_OBJ);
+            String mainName = name.getString(JSONStructure.NAME.MAIN_NAME);
+            JSONArray alsoKnownAsArray = name.getJSONArray(JSONStructure.NAME.ALSO_KNOWN_AS_ARRAY);
 
-        return null;
+            List<String> alsoKnownAs = new ArrayList<>();
+
+            for(int i=0;i<alsoKnownAsArray.length();i++) {
+                alsoKnownAs.add(alsoKnownAsArray.getString(i));
+            }
+
+            String placeOfOrigin = sandwich.getString(JSONStructure.PLACE_OF_ORIGIN);
+            String description = sandwich.getString(JSONStructure.DESCRIPTION);
+            String imageUrl = sandwich.getString(JSONStructure.IMAGE_URL);
+            JSONArray ingredientsJson = sandwich.getJSONArray(JSONStructure.INGREDIENTS_ARRAY);
+            List<String> ingredients = new ArrayList<>();
+
+            for(int i=0;i<ingredientsJson.length();i++) {
+                ingredients.add(ingredientsJson.getString(i));
+            }
+
+            return new Sandwich(
+                    mainName,
+                    alsoKnownAs,
+                    placeOfOrigin,
+                    description,
+                    imageUrl,
+                    ingredients
+            );
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+
     }
 }
