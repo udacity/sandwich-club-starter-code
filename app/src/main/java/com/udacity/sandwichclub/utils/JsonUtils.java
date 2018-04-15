14ed9283d50ca3be61976781bf2ba11352ef1671
Francesco Reale <francescoa.reale@gmail.com>
2018-04-16 01:27:25 +0200

    feat: added implementation of JsonUtils () - UpdateUI() - modified DetailActivity
    Now each object sandwich is created from the parsing of a jsonObject.
    Than thanks the data binding it is possible to see the details of a
    sandwich selected in the detail activity. The DatailActivity.xml has
    been modified to support data binding

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,56 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        try {
+            JSONObject object = new JSONObject(json);
+            String SandwichName = "";
+            String placeOfOrigin = "";
+            String description = "";
+            String image = "";
+            ArrayList<String> alsoKnownAs = new ArrayList<>();
+            ArrayList<String> ingredients = new ArrayList<>();
+
+            if (object.has("name")) {
+                JSONObject nameObj = object.getJSONObject("name");
+
+                if (nameObj.has("mainName"))
+                    SandwichName = nameObj.getString("mainName");
+
+                if (nameObj.has("alsoKnownAs")) {
+                    JSONArray alternativeNames = nameObj.getJSONArray("alsoKnownAs");
+                    for (int i=0; i<alternativeNames.length(); i++)
+                        alsoKnownAs.add(alternativeNames.getString(i));
+                }
+            }
+
+            if (object.has("ingredients")) {
+                JSONArray ingr = object.getJSONArray("ingredients");
+                for (int i=0; i<ingr.length(); i++)
+                    ingredients.add(ingr.getString(i));
+            }
+
+            if (object.has("description"))
+                description = object.getString("description");
+
+            if (object.has("placeOfOrigin"))
+                placeOfOrigin = object.getString("placeOfOrigin");
+
+            if (object.has("image"))
+                image = object.getString("image");
+
+            return new Sandwich(SandwichName, alsoKnownAs, placeOfOrigin, description, image, ingredients);
+        } catch (JSONException e) {
+            return null;
+        }
+
     }
 }
