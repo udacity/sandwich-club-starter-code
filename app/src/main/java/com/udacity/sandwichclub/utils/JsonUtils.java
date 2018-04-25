6b2822cba7eaec32fb537cff1394626bf401ae0f
Ryan Sherry <rsherrycomputers@gmail.com>
2018-04-25 07:08:19 -0700

    Finished basic layout and Sandwich JSON Parser

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,40 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Collections;
+import java.util.List;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+
+        Sandwich newSandwich = new Sandwich();
+
+        try {
+            //Create JSONObject from json String that was passed in
+
+            JSONObject jsonSandwichObject = new JSONObject(json);
+
+            //Store the values from JSONObject to use when creating a new sandwich
+
+            String mainName = jsonSandwichObject.getString("mainName");
+            List<String> alsoKnownAs = Collections.singletonList(jsonSandwichObject.getString("alsoKnownAs"));
+            String placeOfOrigin = jsonSandwichObject.getString("placeOfOrigin");
+            String description = jsonSandwichObject.getString("description");
+            String image = jsonSandwichObject.getString("image");
+            List<String> ingredients = Collections.singletonList(jsonSandwichObject.getString("ingredients"));
+
+            //Create new sandwich object using values above
+
+            newSandwich = new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients);  
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return newSandwich;
     }
 }
