31f197a9c45ee9b455b3a1c9c7a1b072f15d128e
Rakesh Konduru <rakeshpraneeth@gmail.com>
2018-04-28 17:08:01 -0400

    * Added DataBinding. * Designed Detail Screen. * Parsing Json String.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -1,26 +1,29 @@
 package com.udacity.sandwichclub;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
+import com.udacity.sandwichclub.databinding.ActivityDetailBinding;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    ActivityDetailBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
-
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_detail);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -43,10 +46,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
-        Picasso.with(this)
-                .load(sandwich.getImage())
-                .into(ingredientsIv);
+        populateUI(sandwich);
 
         setTitle(sandwich.getMainName());
     }
@@ -56,7 +56,39 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
 
+        if(sandwich !=null) {
+            binding.alsoKnownTv.setText(convertListToString(sandwich.getAlsoKnownAs()));
+            binding.originTv.setText(sandwich.getPlaceOfOrigin().isEmpty() ? getString(R.string.detail_not_availbale) : sandwich.getPlaceOfOrigin());
+            binding.descriptionTv.setText(sandwich.getDescription().isEmpty() ? getString(R.string.detail_not_availbale) : sandwich.getDescription());
+            binding.ingredientsTv.setText(convertListToString(sandwich.getIngredients()));
+
+            Picasso.with(this)
+                    .load(sandwich.getImage())
+                    .placeholder(R.drawable.placeholder)
+                    .into(binding.imageIv);
+
+        }
+    }
+
+    private String convertListToString(List<String> list){
+
+        if(list !=null && list.size() > 0){
+
+            int count = 0;
+            StringBuilder builder = new StringBuilder("");
+
+            for(String string : list){
+                if(count > 0){
+                    builder.append(", ");
+
+                }
+                builder.append(string);
+                count++;
+            }
+            return builder.toString();
+        }
+        return getString(R.string.detail_not_availbale);
     }
 }
