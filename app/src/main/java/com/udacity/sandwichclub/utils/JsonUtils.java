8fc8b92bcc2846e78fcb284afb39be64a433c737
mrk-han <mark.han@willowtreeapps.com>
2018-05-01 13:16:57 -0400

    add basic notation

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -26,7 +26,10 @@ public class JsonUtils {
 
 
     public static Sandwich parseSandwichJson(String json) {
+        // Create our sandwich
         Sandwich sandwich = new Sandwich();
+
+        // Json sandwich anyone? We are manually parsing the json here by using the default json.org library
         try {
             JSONObject jSondwich = new JSONObject(json);
             JSONObject jSondwichName = new JSONObject(jSondwich.getString(SANDWICH_NAME));
@@ -50,12 +53,12 @@ public class JsonUtils {
             sandwich.setDescription(jSondwich.getString(SANDWICH_DESCRIPTION));
             sandwich.setImage(jSondwich.getString(SANDWICH_IMAGE));
             sandwich.setIngredients(ingredientList);
-            Log.i(TAG, "sandwich.getMainName: " + sandwich.getMainName());
-            Log.i(TAG, "sandwich.getAlsoKnownAs: " + sandwich.getAlsoKnownAs());
-            Log.i(TAG, "sandwich.getPlaceOfOrigin: " + sandwich.getPlaceOfOrigin());
-            Log.i(TAG, "sandwich.getDescription: " + sandwich.getDescription());
-            Log.i(TAG, "sandwich.getImage: " + sandwich.getImage());
-            Log.i(TAG, "sandwich.getIngredients: " + sandwich.getIngredients());
+            Log.d(TAG, "sandwich.getMainName: " + sandwich.getMainName());
+            Log.d(TAG, "sandwich.getAlsoKnownAs: " + sandwich.getAlsoKnownAs());
+            Log.d(TAG, "sandwich.getPlaceOfOrigin: " + sandwich.getPlaceOfOrigin());
+            Log.d(TAG, "sandwich.getDescription: " + sandwich.getDescription());
+            Log.d(TAG, "sandwich.getImage: " + sandwich.getImage());
+            Log.d(TAG, "sandwich.getIngredients: " + sandwich.getIngredients());
 
         } catch (final JSONException e) {
             e.printStackTrace();
