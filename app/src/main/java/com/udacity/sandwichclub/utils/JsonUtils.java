34fae7d4f8958e1b0273cd53ba77fab9e38755ab
coreen <coreeny@cs.washington.edu>
2018-05-02 09:14:46 -0700

    add spacing for dev readability

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -14,21 +14,26 @@ public class JsonUtils {
     public static Sandwich parseSandwichJson(String json) {
         try {
             JSONObject sandwich = new JSONObject(json);
+
             JSONObject name = sandwich.getJSONObject("name");
             String mainName = name.getString("mainName");
+
             List<String> alsoKnownAs = new ArrayList<>();
             JSONArray akaArray = name.getJSONArray("alsoKnownAs");
             for (int i = 0; i < akaArray.length(); i++) {
                 alsoKnownAs.add(akaArray.getString(i));
             }
+
             String placeOfOrigin = sandwich.getString("placeOfOrigin");
             String description = sandwich.getString("description");
             String image = sandwich.getString("image");
+
             List<String> ingredients = new ArrayList<>();
             JSONArray ingredientArray = sandwich.getJSONArray("ingredients");
             for (int j = 0; j < ingredientArray.length(); j++) {
                 ingredients.add(ingredientArray.getString(j));
             }
+
             return new Sandwich(
                     mainName,
                     alsoKnownAs,
