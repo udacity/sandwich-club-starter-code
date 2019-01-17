ae54faa12dea91ce05fd3c82715bdf337818033c
심현용 <jonesn5508@gmail.com>
2019-01-17 16:09:12 +0900

    Populate all fields in the layout accordingly

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -43,7 +46,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +59,21 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        ((TextView) findViewById(R.id.also_known_tv)).setText(getListToString(sandwich.getAlsoKnownAs()));
 
+        ((TextView) findViewById(R.id.ingredients_tv)).setText(getListToString(sandwich.getIngredients()));
+
+        ((TextView) findViewById(R.id.origin_tv)).setText(sandwich.getPlaceOfOrigin());
+
+        ((TextView) findViewById(R.id.description_tv)).setText(sandwich.getDescription());
+    }
+
+    private String getListToString(List<String> lists) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < lists.size(); i++) {
+            stringBuilder.append(lists.get(i)).append("\n");
+        }
+        return stringBuilder.toString();
     }
 }
